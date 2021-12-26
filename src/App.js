cb4d48a549b009c1c2f6d549e719ffc69815cb7f
sbk201 <siuym4mym4@gmail.com>
2021-12-26 22:30:26 +0800

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn And Read React
         </a>
       </header>
     </div>
